81bd0c9387e99c06a8365f8980aa5e5fbbe426fe
Gyumin_Kim <145199011+eg1mb@users.noreply.github.com>
2025-05-02 03:57:02 +0900

    Update event.d.ts

--- kakaomap-api-react/app/types/kakao/event.d.ts
+++ kakaomap-api-react/app/types/kakao/event.d.ts
@@ -3,9 +3,9 @@ namespace kakao {
     namespace event {
       type EventTarget = kakao.maps.Map | kakao.maps.Marker;
 
-      function addListener(target:EventTarget, type:stirng, handler:function): void;
-      function removeListener(target:EventTarget, type:stirng, handler:function): void;
-      function trigger(target:EventTarget, type:stirng, data:unknown): void;
+      function addListener(target:EventTarget, type:string, handler:function): void;
+      function removeListener(target:EventTarget, type:string, handler:function): void;
+      function trigger(target:EventTarget, type:string, data:unknown): void;
       function preventMap(): void;
 
       interface MouseEvent {
